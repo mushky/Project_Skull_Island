71d5598361fa6193edf18d88e9fe3a8b2f88acdf
Ali Zaidi <msazaidi@gmail.com>
2016-01-30 12:04:31 -0800

    Added Basic Enemy Movement Script

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
+	public Transform target;
+	public float speed = 3f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Update(){
+
+	//rotate to look at the player
+	transform.LookAt(target.position);
+	transform.Rotate(new Vector3(0,-90,0),Space.Self);
+	transform.Translate(new Vector3(speed* Time.deltaTime,0,0));
 	
 	}
 }
