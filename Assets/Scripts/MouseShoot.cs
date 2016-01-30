da67a36c4e55b1f86a8c0c3b0fe7c4d0666830b9
Ali Zaidi <msazaidi@gmail.com>
2016-01-30 14:39:31 -0800

    Updates and stuffs

--- Assets/Scripts/MouseShoot.cs
+++ Assets/Scripts/MouseShoot.cs
@@ -6,23 +6,19 @@ public class MouseShoot : MonoBehaviour {
 	public GameObject bullet;
 	public float speed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButtonDown(0)) 
+		{
 			Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
 			Vector2 myPos = new Vector2(transform.position.x,transform.position.y);
 			Vector2 direction = target - myPos;
+
 			direction.Normalize();
+
 			Quaternion rotation = Quaternion.Euler(0,0,Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90) ;
 			GameObject projectile = (GameObject) Instantiate(bullet,myPos, rotation);
 			projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
-
-
-			}
+		}
 	}
 }
