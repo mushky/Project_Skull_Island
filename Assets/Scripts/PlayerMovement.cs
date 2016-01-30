50bd9a33cb8f4f1845c56504951267ce8c0ce255
Ali Zaidi <msazaidi@gmail.com>
2016-01-30 14:51:50 -0800

    Update for merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,34 +7,14 @@ public class PlayerMovement : MonoBehaviour {
 	Animator anim;
 	public float speed = 5.0f;
 
-	// Shooting Mechanics
-	public float fireRate = 1f;
-	public float delay = 1f;
-	public GameObject shot;
-	public Transform shotSpawn;
-
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
 	}
 		
-	void Fire ()
-	{
-		Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-	}
-
 	void Update () {
 		Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
 		rigidbody.MovePosition(rigidbody.position + movementVector * speed * Time.deltaTime);
-
-		if (Input.GetKey("space"))
-		{
-			Fire();
-		
 	}
-
-
-		}
-
-	}
+				
+}
