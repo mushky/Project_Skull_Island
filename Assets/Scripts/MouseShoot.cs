2eaa0d7ad3f2abb581b0f5d5d9dfb0abd7ab0b96
Ali Zaidi <msazaidi@gmail.com>
2016-01-30 14:26:32 -0800

    Bullet Instantiate from Center of Hero

--- Assets/Scripts/MouseShoot.cs
+++ Assets/Scripts/MouseShoot.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletBehaviors : MonoBehaviour {
+public class MouseShoot : MonoBehaviour {
 
 	public GameObject bullet;
 	public float speed = 5.0f;
@@ -15,7 +15,7 @@ public class BulletBehaviors : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-			Vector2 myPos = new Vector2(transform.position.x,transform.position.y + 1);
+			Vector2 myPos = new Vector2(transform.position.x,transform.position.y);
 			Vector2 direction = target - myPos;
 			direction.Normalize();
 			Quaternion rotation = Quaternion.Euler(0,0,Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90) ;
