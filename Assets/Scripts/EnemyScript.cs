40abf706b44ede4b90ee1b137a92d33490de2896
Gezus10k <chanh.vinh.le@gmail.com>
2016-01-30 19:06:52 -0800

    grabbing map

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyScript : MonoBehaviour {
 	public int hitPoints = 30;
 	public float speed = 3f;
+	public Transform Player;
 
 	void OnTriggerEnter2D(Collider2D collide)
 	{
@@ -12,6 +13,12 @@ public class EnemyScript : MonoBehaviour {
 			hitPoints -= 10;
 			Debug.Log("Enemy Hit by Player Bullet");
 		}
+		if (collide.gameObject.tag == "Player") 
+		{
+			transform.LookAt(Player);
+			transform.position += Vector3.up * Time.deltaTime * speed;
+		}
+				
 	}
 	void Update () {
 		if (hitPoints >= 0 && hitPoints <= 10) {
