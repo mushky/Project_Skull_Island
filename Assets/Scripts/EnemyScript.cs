e0ff8b94579d6ff98809a0a7cbb4bc176a676427
Ali Zaidi <msazaidi@gmail.com>
2016-01-30 19:16:49 -0800

    Enemy Follow Code implemented

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class EnemyScript : MonoBehaviour {
 	public int hitPoints = 30;
 	public float speed = 3f;
+	public Transform player;
+
+	private float Distance = 2f;
+	private float Range;
+
+	void Start()
+	{
+		Debug.Log(Range);
+	}
 
 	void OnTriggerEnter2D(Collider2D collide)
 	{
@@ -13,17 +22,25 @@ public class EnemyScript : MonoBehaviour {
 			Debug.Log("Enemy Hit by Player Bullet");
 		}
 	}
+		
 	void Update () {
 		if (hitPoints >= 0 && hitPoints <= 10) {
 			speed = 0f;
-//			anim = GetComponent<Animator> ();
+			// anim = GetComponent<Animator> ();
 			Debug.Log ("Enemy is stunned");
 		}
 
 		if (hitPoints <= 0) {
 			Destroy (this.gameObject);
 			Debug.Log ("Dead");
+		}
 
+		Range = Vector2.Distance(transform.position, player.position);
+
+		if (Range <= Distance)
+		{
+			Debug.Log(Range);
+			transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);		
 		}
 
 	}
