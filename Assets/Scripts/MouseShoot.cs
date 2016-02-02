089e5139a7b21d41faa188ee89e391810abfd5a5
Ali Zaidi <msazaidi@gmail.com>
2016-02-01 16:52:56 -0800

    Refactored MouseShoot Script

--- Assets/Scripts/MouseShoot.cs
+++ Assets/Scripts/MouseShoot.cs
@@ -1,50 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseShoot : MonoBehaviour {
-
+public class MouseShoot : MonoBehaviour 
+{
 	public GameObject bullet;
-	public float speed = 5.0f;
 	public GameObject net;
+
+	public float speed = 5.0f;
 	public float oldspeed = 6.0f;
 	public bool pause = true;
+
 	Animator anim;
 
-	void Start () {
+	void Start () 
+	{
 		anim = GetComponent<Animator>();
 	}
 
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) 
 		{
-			Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-			Vector2 myPos = new Vector2(transform.position.x,transform.position.y);
-			Vector2 direction = target - myPos;
-
-			direction.Normalize();
-
-			Quaternion rotation = Quaternion.Euler(0,0,Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90) ;
-			GameObject projectile = (GameObject) Instantiate(bullet,myPos, rotation);
-			projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
-
+			MouseFire(bullet);
+			AnimatePlayer();
 		}
 
-		if (Input.GetMouseButtonDown(1)) {
-			Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-			Vector2 myPos = new Vector2(transform.position.x,transform.position.y);
-			Vector2 direction = target - myPos;
-			direction.Normalize();
-			Quaternion rotation = Quaternion.Euler(0,0,Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90) ;
-			GameObject projectile = (GameObject) Instantiate(net, myPos, rotation);
-			projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
-
-			// Attack Animation
-			anim.Play("PlayerAttack");
-			StartCoroutine(AnimationPause());
-			anim.Play("PlayerIdle");
-			StartCoroutine(AnimationPause());
-
+		if (Input.GetMouseButtonDown(1)) 
+		{
+			MouseFire(net);
+			AnimatePlayer();
 		}
+	}
+
+	void MouseFire(string weapon)
+	{
+		Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+		Vector2 myPos = new Vector2(transform.position.x,transform.position.y);
+		Vector2 direction = target - myPos;
+		direction.Normalize();
+		Quaternion rotation = Quaternion.Euler(0,0,Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90);
+		GameObject projectile = (GameObject) Instantiate(weapon,myPos, rotation);
+		projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+
+	void AnimatePlayer()
+	{
+		// Attack Animation
+		anim.Play("PlayerAttack");
+		StartCoroutine(AnimationPause());
+		anim.Play("PlayerIdle");
+		StartCoroutine(AnimationPause());
 	}
 
 	// Cheap Animation Change Fix
